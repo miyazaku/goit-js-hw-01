495876bdc25d19c80a344ed1761b776ecd8bf88d
miyazaku <solomiyazaku@icloud.com>
2025-07-09 22:28:21 +0200

    add all

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,7 @@
 function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+    const message = `"Shipping to ${country} will cost ${totalPrice} credits"`;
+    return message;
     
 }
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
